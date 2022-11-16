67423b8226560d5ced2db16016558ec29290b0ba
Arunvarma1862 <arunbabu120894@gmail.com>
2022-11-16 20:52:56 +0530

    firs

---
+++ src/testngproject/pratice.java
@@ -0,0 +1,16 @@
+package testngproject;
+
+import org.testng.annotations.Test;
+
+public class pratice {
+	
+	
+	@Test
+	
+	public void abcd() {
+		System.out.println("hello");
+	}
+    public static void main(String[] args) {
+		System.out.println("hello");
+	}
+}
